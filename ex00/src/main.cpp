066b55a2025ff3471aa319d09ebd90d49770d9fd
um4ss <sliziard@icloud.com>
2026-02-21 22:10:14 +0100

    feat(ex00): update Makefile and add main.cpp for BitcoinExchange execution

---
+++ ex00/src/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/21 21:56:43 by sliziard          #+#    #+#             */
+/*   Updated: 2026/02/21 22:09:46 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	try
+	{
+		BitcoinExchange btc;
+
+		btc.loadDatabase("assets/data.csv");
+		btc.processInput(argc > 1 ? argv[1] : "");
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
+

c1586fcefc1f6dad9b4df806c4065c72d396ef75
um4ss <sliziard@icloud.com>
2026-02-23 18:11:11 +0100

    feat(ex01): add RPN class implementation with evaluate method

---
+++ ex01/src/RPN.cpp
@@ -0,0 +1,145 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/23 17:15:35 by sliziard          #+#    #+#             */
+/*   Updated: 2026/02/23 18:10:06 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RPN.hpp"
+#include <cctype>
+#include <cstdint>
+#include <list>
+#include <stack>
+#include <stdexcept>
+
+// ============================================================================
+// Construction / Destruction
+// ============================================================================
+
+const RPN::Operation RPN::_ops[4] =
+{
+	{'+', &RPN::add},
+	{'-', &RPN::sub},
+	{'*', &RPN::mul},
+	{'/', &RPN::div}
+};
+
+RPN::RPN(): _lifo()
+{}
+
+RPN::RPN(const RPN &other): _lifo(other._lifo)
+{}
+
+RPN::~RPN()
+{}
+
+// ============================================================================
+// Operators
+// ============================================================================
+
+RPN& RPN::operator=(const RPN &other)
+{
+	if (this != &other)
+	{
+		_lifo = other._lifo;
+	}
+	return *this;
+}
+
+// ============================================================================
+// Methods
+// ============================================================================
+
+static inline int32_t	next(std::stack<int32_t, std::list<int32_t> > &st)
+{
+	if (st.empty())
+		throw RPN::InvalidExpression("not enough operands");
+	int32_t	x = st.top();
+	st.pop();
+	return x;
+}
+
+int32_t	RPN::evaluate(const std::string &expr)
+{
+	OpFunc	op;
+	int32_t	a;
+	int32_t	b;
+
+	clear();
+	for (size_t i = 0; i < expr.size(); ++i)
+	{
+		if (std::isdigit(expr[i]))
+		{
+			if (i + 1 < expr.size() && std::isdigit(expr[i + 1]))
+				throw InvalidExpression("numbers must be single digits (0-9)");
+
+			_lifo.push(expr[i] - '0');
+			continue;
+		}
+		if (std::isspace(expr[i]))
+			continue;
+
+		op = getOp(expr[i]);
+		if (!op)
+			throw InvalidOperator(expr[i]);
+
+		b = next(_lifo);
+		a = next(_lifo);
+
+		_lifo.push(op(a, b));
+	}
+
+	if (_lifo.size() != 1)
+		throw InvalidExpression("invalid expression format");
+	return next(_lifo);
+}
+
+void	RPN::clear(void)
+{
+	while (!_lifo.empty())
+		_lifo.pop();
+}
+
+// ========================================================================
+// Static helpers
+// ========================================================================
+
+RPN::OpFunc	RPN::getOp(char symbol)
+{
+	for (size_t i = 0; i < sizeof(_ops) / sizeof(_ops[0]); ++i)
+	{
+		if (_ops[i].symbol == symbol)
+			return _ops[i].func;
+	}
+	return NULL;
+}
+
+// ---- Operations ----
+int32_t	RPN::add(int32_t a, int32_t b) { return a + b; }
+int32_t	RPN::sub(int32_t a, int32_t b) { return a - b; }
+int32_t	RPN::mul(int32_t a, int32_t b) { return a * b; }
+int32_t	RPN::div(int32_t a, int32_t b)
+{
+	if (b == 0)
+		throw std::runtime_error("division by zero");
+	return a / b;
+}
+
+// ========================================================================
+// Exception
+// ========================================================================
+
+RPN::InvalidOperator::InvalidOperator(char op)
+	: std::runtime_error(
+		"invalid operator: '" + std::string(1, op) + '\''
+	)
+{}
+
+RPN::InvalidExpression::InvalidExpression(const std::string &msg)
+	: std::runtime_error(msg)
+{}
---
+++ ex01/src/RPN.hpp
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/23 15:26:00 by sliziard          #+#    #+#             */
+/*   Updated: 2026/02/23 18:10:18 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __RPN_HPP__
+# define __RPN_HPP__
+
+# include <cstdint>
+# include <list>
+# include <stack>
+# include <stdexcept>
+# include <stdint.h>
+# include <string>
+
+class RPN {
+
+private:
+	typedef int32_t	(*OpFunc)(int32_t, int32_t);
+	struct Operation
+	{
+		char	symbol;
+		OpFunc	func;
+
+	};
+
+	std::stack<int32_t, std::list<int32_t> >	_lifo;
+	static const Operation						_ops[4];
+
+public:
+
+	// ========================================================================
+	// Construction / Destruction
+	// ========================================================================
+	RPN();
+	RPN(const RPN &other);
+	~RPN();
+
+	// ========================================================================
+	// Operators
+	// ========================================================================
+	RPN& operator=(const RPN &other);
+
+	// ========================================================================
+	// Public Method
+	// ========================================================================
+	int32_t			evaluate(const std::string &expr);
+	void			clear(void);
+
+	// ========================================================================
+	// Exception
+	// ========================================================================
+	class InvalidOperator : public std::runtime_error {
+	public:
+		InvalidOperator(char op);
+	};
+
+	class InvalidExpression : public std::runtime_error {
+	public:
+		InvalidExpression(const std::string &msg);
+	};
+
+private:
+	// ========================================================================
+	// Static helpers
+	// ========================================================================
+	static OpFunc	getOp(char symbol);
+
+	// --- Operations ----
+	static int32_t	add(int32_t a, int32_t b);
+	static int32_t	sub(int32_t a, int32_t b);
+	static int32_t	mul(int32_t a, int32_t b);
+	static int32_t	div(int32_t a, int32_t b);
+};
+
+
+#endif /* __RPN_HPP__ */
